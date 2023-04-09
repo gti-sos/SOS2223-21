9d0150d0aaa88fc7fc9fc675426a97d7780d2100
PABLO VERA LOPEZ <pabverlop1@alum.us.es>
2023-04-09 18:59:26 +0200

    ocult

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { loadBackend_jorge } from './backend/jfs/modulo-jfs_v2.js';
-var modulo_pablo = require("./backend/pvl/modulo-pvl.js");
+//var modulo_pablo = require("./backend/pvl/modulo-pvl.js");
 //var modulo_sete = require("./backend/src/modulo-src.js");
 import { handler } from "./frontend/build/handler.js";
 
@@ -12,7 +12,7 @@ var port = process.env.PORT || 12345;
 app.use(express.json());
 //BACKEND
 loadBackend_jorge(app);
-modulo_pablo.api(app);
+//modulo_pablo.api(app);
 //modulo_sete.api(app);
 
 app.use(handler); //configurador svelte
