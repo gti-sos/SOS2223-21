f09c71ee91376b24080381d86dba2597d78f8611
Setefilla <setruacal@alum.us.es>
2023-04-17 11:03:12 +0200

    commit sete

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ import { loadBackend_jorge } from './backend/jfs/modulo-jfs_v2.js';
 import { loadBackend_jorge_v1 } from './backend/jfs/modulo-jfs.js';
 import { LoadModulo_Pablo } from './backend/pvl/modulo-pvl.js';
 import { LoadModulo_Pablo_v2 } from './backend/pvl/modulo-pvl-v2.js';
-//var modulo_sete = require("./backend/src/modulo-src.js");
-//import { LoadModulo_Pablo } from './backend/pvl/modulo-pvl.js';
 import { loadBackend_src_v2 } from './backend/src/modulo-src_v2.js';
+//import { LoadModulo_Pablo } from './backend/pvl/modulo-pvl.js';
 
 var app = express();
 
@@ -19,7 +18,6 @@ loadBackend_jorge(app);
 loadBackend_jorge_v1(app);
 LoadModulo_Pablo(app);
 LoadModulo_Pablo_v2(app);
-//modulo_sete.api(app);
 loadBackend_src_v2(app);
 
 app.use(handler); //configurador svelte
