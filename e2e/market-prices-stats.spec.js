fa03462c5f34c986ee9219bb03cd37fbcf012ca9
PABLO VERA LOPEZ <pabverlop1@alum.us.es>
2023-05-19 05:54:39 +0200

    e2e

--- e2e/market-prices-stats.spec.js
+++ e2e/market-prices-stats.spec.js
@@ -11,18 +11,6 @@ test('market-prices-stats create data works', async ({ page }) => {
     await page.waitForSelector('Table');
     await expect((await page.$$('table tr')).length).toBeGreaterThan(1);
 });
-test('market-prices-stats delete single resource works', async ({ page }) => {
-    await page.goto('http://localhost:12345/market-prices-stats');
-    
-    // Guarda la cantidad inicial de filas en la tabla
-    const initialRowCount = (await page.$$('table tr')).length;
-    await page.getByText('Borrar').click();
-    await page.waitForSelector('Table');
-    // Obtiene la nueva cantidad de filas en la tabla después del borrado
-    const newRowCount = (await page.$$('table tr')).length;
-    // Verifica que la cantidad de filas se haya reducido en 1
-    await expect(newRowCount).toEqual(initialRowCount - 1);
-  });
 test('market-prices-stats delete all data works', async ({ page }) => {
     await page.goto('http://localhost:12345/market-prices-stats');
     await page.getByText('Borrar recursos').click();
--- e2e/workingplaces-stats.spec.js
+++ e2e/workingplaces-stats.spec.js
@@ -11,6 +11,18 @@ test('workingplaces-stats create data works', async ({ page }) => {
     await page.waitForSelector('Table');
     await expect((await page.$$('table tr')).length).toBeGreaterThan(1);
 });
+test('workingplaces-stats delete single resource works', async ({ page }) => {
+    await page.goto('http://localhost:12345/workingplaces-stats');
+    
+    // Guarda la cantidad inicial de filas en la tabla
+    const initialRowCount = (await page.$$('table tr')).length;
+    await page.getByText('Borrar').click();
+    await page.waitForSelector('Table');
+    // Obtiene la nueva cantidad de filas en la tabla después del borrado
+    const newRowCount = (await page.$$('table tr')).length;
+    // Verifica que la cantidad de filas se haya reducido en 1
+    await expect(newRowCount).toEqual(initialRowCount - 1);
+  });
 test('workingplaces-stats delete all data works', async ({ page }) => {
     await page.goto('http://localhost:12345/workingplaces-stats');
     await page.getByText('Borrar recursos').click();
